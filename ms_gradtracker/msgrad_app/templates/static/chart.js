a6c23b0ce9cfb3d634195ad0ec8eaea034aa459b
Marilyn Page <mpage882@gmail.com>
2023-02-28 09:32:58 -0600

    password reset

---
+++ ms_gradtracker/msgrad_app/templates/static/chart.js
@@ -0,0 +1,11 @@
+// let pie = document.getElementById("chart").getContext("2d");
+// let labels = document.getElementsByName({labels})
+
+
+// let pieChart = new Chart(pie, {
+//     type: "pie",
+//     data: {
+//         labels:[]
+//     }
+// })
+  
